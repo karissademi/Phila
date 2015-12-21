d39e4c59898d4629e2ecf21671bc3a97583f7fa5
franciscogalarza <franciscogalarza@franciscogalarza.com>
2015-12-20 23:05:04 -0500

    Azure

--- Web/App/Streets/Phila.Web.App.Streets.Closures/Scripts/app/app.js
+++ Web/App/Streets/Phila.Web.App.Streets.Closures/Scripts/app/app.js
@@ -43,7 +43,7 @@ function AppViewModel() {
 
 
     self.totalPermitsFound = ko.observable();
-    self.apiUrl = "http://localhost/Phila.Web.Api.Streets/";//"https://phila.azurewebsites.net/"; //   
+    self.apiUrl = "https://phila.azurewebsites.net/"; // "http://localhost/Phila.Web.Api.Streets/";//  
     self.streetCode = "";
     self.fromStreets = ko.observableArray();
 
